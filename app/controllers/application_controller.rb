be40e4aed05630496eb1319ab946be2c16b1edab
+ <+>
2022-09-08 01:25:22 +0300

    implemented post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,17 @@ class ApplicationController < Sinatra::Base
     record = Record.all
     record.to_json
   end
+
+  get '/record/:id' do
+    record = Record.find(params[:id])
+    record.to_json
+  end
+
+  post '/record' do
+    new_record = Record.create(
+      book_author: params[:book_author],  student_age: params[:student_age], student_gender: params[:student_gender],
+      student_contact: params[:student_contact], student_email: params[:student_email], book_id: params[:book_id], student_id: params[:student_id]
+    )
+    new_record.to_json
+  end
 end
