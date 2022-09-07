f359fc16e24a41dd180ef4d75b667fc0fc1896a3
+ <+>
2022-09-08 01:45:42 +0300

    implemented POST

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,4 +37,69 @@ class ApplicationController < Sinatra::Base
     delete_record.destroy
     delete_record.to_json
   end
+
+  get '/book' do
+    get_book = Book.all.order(:asc)
+    get_book.to_json
+  end
+
+  get '/book/:id' do
+    get_book = Book.find(params[:id])
+    get_book.to_json
+  end
+
+  post '/book' do
+    new_book = Book.create(
+      book_title: params[:book_title]
+    )
+    new_book.to_json
+  end
+
+  patch '/book/:id' do
+    update_book = Book.find(params[:id])
+    update_book.update(
+      book_title: params[:book_title]
+    )
+    update_book.to_json
+  end
+
+  delete '/book/:id' do
+    delete_book = Book.find(params[:id])
+    delete_book.destroy
+    delete_book.to_json
+  end
+
+
+
+  get '/student' do
+    get_student = Student.all.order(:asc)
+    get_student.to_json
+  end
+
+  get '/student/:id' do
+    get_student = Student.find(params[:id])
+    get_student.to_json
+  end
+
+  post '/student' do
+    new_student = Student.create(
+      name: params[:name]
+    )
+    new_student.to_json
+  end
+
+  patch '/student/:id' do
+    update_student = Student.find(params[:id])
+    update_student.update(
+      name: params[:name]
+    )
+    update_student.to_json
+  end
+
+  delete '/student/:id' do
+    delete_student = Student.find(params[:id])
+    delete_student.destroy
+    delete_student.to_json
+  end
+
 end
